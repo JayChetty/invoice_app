1f715c3968870ae94326f5ba04a1b174c312e29f
JayChetty <jaypaulchetty@gmail.com>
2013-08-14 13:27:16 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InvoiceApp::Application.config.secret_token = 'd3e796e6cec3ecf658622124bfa902fc4e615d8713c3c199fabb80fe885c57cd6a91f975532048eeee0ac80fb5b3fb8a29c6e596bf691d53ab54dc7acec7a081'
