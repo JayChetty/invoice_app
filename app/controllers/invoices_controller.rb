e6d7794caa4bf342139a8ff344f2b67423693be3
JayChetty <jaypaulchetty@gmail.com>
2013-08-20 21:29:28 +0100

    sortable columns

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,6 +1,6 @@
 class InvoicesController < ApplicationController
   before_filter :require_type, only: :index 
-
+  helper_method :sort_column, :sort_direction
   def index
     @title = "Invoices | #{params[:type].capitalize}"
 
@@ -16,8 +16,10 @@ class InvoicesController < ApplicationController
       typestring = 'ACCPAY'
     end
 
+    #order = params[:sort] || :due_date
+
     if params[:status]
-      @invoices = Invoice.where(invoice_type: typestring, status: params[:status].upcase)
+      @invoices = Invoice.where(invoice_type: typestring, status: params[:status].upcase).order("#{sort_column} #{sort_direction}")
     else
       @invoices = Invoice.where(invoice_type: typestring)
     end
@@ -34,5 +36,14 @@ class InvoicesController < ApplicationController
     def require_type 
       redirect_to invoices_all_path('receivable') unless params[:type] 
     end
+
+    def sort_column
+      Invoice.column_names.include?(params[:sort]) ? params[:sort] : "due_date"
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
+    end
+
 
 end
